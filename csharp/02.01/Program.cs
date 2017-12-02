25dc7ff5c38b0b3901634dd1bbff1622116ece76
Robbie Luijben <diggingforfire@gmail.com>
2017-12-02 14:57:57 +0100

    Day 2

---
+++ csharp/02.01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02._01
+{
+	class Program
+	{
+		static void Main()
+		{
+			var result = File.ReadAllLines("input.txt")
+				.Select(row => row.Split('\t', StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse).ToArray())
+				.Sum(n => n.Max() - n.Min());
+
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ csharp/02.02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02._02
+{
+	class Program
+	{
+		static void Main()
+		{
+			var result = File.ReadAllLines("input.txt")
+				.Select(row => row.Split('\t', StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse).ToArray())
+				.Sum(n => n.SelectMany(p => n.Where(o => p % o == 0 && p != o).Select(o => p / o)).First());
+
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+	}
+}
