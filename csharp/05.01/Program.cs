37e4bb24e80b0a4b7beb3948de76daf5d11ff1b6
Robbie Luijben <diggingforfire@gmail.com>
2017-12-05 22:23:56 +0100

    Day 5

---
+++ csharp/05.01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _05._01
+{
+	class Program
+	{
+		static void Main()
+		{
+			var input =
+				File.ReadAllLines("input.txt")
+				.Select(int.Parse)
+				.ToArray();
+
+			int result = 0;
+
+			try
+			{
+				for (int index = 0;; result++)
+				{
+					index += input[index]++;
+				}
+			}
+			// Instead of proper bounds checking, just to annoy Teun
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine(result);
+				Console.ReadKey();
+			}
+		}
+	}
+}
---
+++ csharp/05.02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _05._02
+{
+	class Program
+	{
+		static void Main()
+		{
+			var input =
+				File.ReadAllLines("input.txt")
+					.Select(int.Parse)
+					.ToArray();
+
+			int result = 0;
+
+			try
+			{
+				for (int index = 0; ; result++)
+				{
+					int oldIndex = index;
+					index += input[index];
+					input[oldIndex] += index - oldIndex >= 3 ? -1 : 1;
+				}
+			}
+			// Instead of proper bounds checking, just to annoy Teun
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine(result);
+				Console.ReadKey();
+			}
+		}
+	}
+}
